78ad72e37e27ac8c6b5db13a88de1a931417c12e
geraldbriones <briones.gerald@gmail.com>
2013-07-16 13:22:44 -0700

    First commit attempt to new repository?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geebook2::Application.config.secret_token = 'eb18ce70b8f71abea84ba1dd8eb2c709becc602918bad79cd106d33c45cb7e0f1b7615b53365826d25e9e6fd594d355b6e6b042816aa3e67222b04002e48f17d'
